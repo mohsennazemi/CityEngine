49d4e1c8b992cdceea51396f31d08e1ad3617c4b
mohsennazemi <mohsen.nazemi.08@gmail.com>
2016-06-14 18:09:16 +0800

    start/end points of the streets: this code finds start/end points based on the average of the common nodes.
    the not precise code to find the start/end point based on trigonometry is also included in the comments at the end of the script.

--- MohsenScript4.py
+++ MohsenScript4.py
@@ -5,11 +5,13 @@ Created on Apr 20, 2016
 '''
 '''''''''''''''''''''''
 Pre-processing:
-Name all the network's segment "segments"
+Name all the network's segments "segment"
 '''''''''''''''''''''''
 
 from scripting import *
 import os.path
+import math
+import operator
 
 # get a CityEngine instance
 ce = CE()
@@ -18,7 +20,7 @@ ce = CE()
 ce.setSelection(ce.getObjectsFrom(ce.scene, ce.withName('segment')))
 segments = ce.getObjectsFrom(ce.selection)
 
-# Constructing OIDs, obtaining objects' vertices, translation to global coordination, and finding segment orientation
+# Constructing OIDs, obtaining objects' vertices, and translation to global coordination
 for i in segments:
 
     segmentsOID = ce.getOID(i)
@@ -57,10 +59,53 @@ for i in segments:
         z2 = (-1) * sidewalk2_init[m + 2]
         sidewalk2.extend([x2, z2, 0.0])
 
-    print segment012
-    print street0
-    print sidewalk1
-    print sidewalk2, '\n'
+    # Obtaining common nodes between street and sidewalks
+
+    segment012t = [tuple(segment012[3 * a:3 * a + 3]) for a in range(len(segment012) // 3)]
+    street0t = [tuple(street0[3 * a:3 * a + 3]) for a in range(len(street0) // 3)]
+    sidewalk1t = [tuple(sidewalk1[3 * a:3 * a + 3]) for a in range(len(sidewalk1) // 3)]
+    sidewalk2t = [tuple(sidewalk2[3 * a:3 * a + 3]) for a in range(len(sidewalk2) // 3)]
+
+    street0_set = set(street0t)
+    sidewalk1_set = set(sidewalk1t)
+    sidewalk2_set = set(sidewalk2t)
+
+    intersection1 = list(street0_set.intersection(sidewalk1_set))
+    intersection2 = list(street0_set.intersection(sidewalk2_set))
+    intersections = intersection1 + intersection2
+
+    # finding streets' mid-points by averaging pedestrian-street common points
+    if len(intersection1) == 2 and len(intersection2) == 2:
+
+        if (intersection1[0][0] + intersection1[0][1]) < (intersection1[1][0] + intersection1[1][1]):
+            min_inter1 = [intersection1[0][0] , intersection1[0][1]]
+            max_inter1 = [intersection1[1][0] , intersection1[1][1]]
+        else:
+            min_inter1 = [intersection1[1][0] , intersection1[1][1]]
+            max_inter1 = [intersection1[0][0] , intersection1[0][1]]
+
+        if (intersection2[0][0] + intersection2[0][1]) < (intersection2[1][0] + intersection2[1][1]):
+            min_inter2 = [intersection2[0][0] , intersection2[0][1]]
+            max_inter2 = [intersection2[1][0] , intersection2[1][1]]
+        else:
+            min_inter2 = [intersection2[1][0] , intersection2[1][1]]
+            max_inter2 = [intersection2[0][0] , intersection2[0][1]]
+    else:
+        print 'No exact 4 points for street ID "', streetsOID, '"', '\n'
+
+    start = [(min_inter1[0] + min_inter2[0]) / 2, (min_inter1[1] + min_inter2[1]) / 2]
+    end = [(max_inter1[0] + max_inter2[0]) / 2, (max_inter1[1] + max_inter2[1]) / 2]
+
+    print 'segment ID:', '"', streetsOID, '"'
+    print 'start:', start
+    print 'end:', end, '\n'
+
+if __name__ == '__main__':
+    pass
+
+    '''
+    # segment_line
+    M = (segment012[4]-segment012[1])/(segment012[3]-segment012[0])
 
     # vertical, horizontal, or diagonal?
     if (segment012[3] - segment012[0]) == 0:
@@ -70,24 +115,6 @@ for i in segments:
     else:
         orientation = 'DIAGONAL'
 
-    street0_set = set(street0)
-    sidewalk1_set = set(sidewalk1)
-    sidewalk2_set = set(sidewalk2)
-
-    intersection1 = list(street0_set.intersection(sidewalk1_set))
-    intersection2 = list(street0_set.intersection(sidewalk2_set))
-    intersections = intersection1 + intersection2
-
-    # obtaining common nodes between street and sidewalks
-    segment012 = [tuple(segment012[3 * i:3 * i + 3]) for i in range(len(segment012) // 3)]
-    street0 = [tuple(street0[3 * i:3 * i + 3]) for i in range(len(street0) // 3)]
-    sidewalk1 = [tuple(sidewalk1[3 * i:3 * i + 3]) for i in range(len(sidewalk1) // 3)]
-    sidewalk2 = [tuple(sidewalk2[3 * i:3 * i + 3]) for i in range(len(sidewalk2) // 3)]
-
-if __name__ == '__main__':
-    pass
-
-    '''
     # eliminating the outlier points for segments with more than 4 common points
     if len(intersections) > 4:
 
@@ -110,8 +137,72 @@ if __name__ == '__main__':
         for n in distances:
             if distances.count(m) == 1:
                 distance = n
+
     '''
-    #for m in distances:
+    '''
+    # Finding the projection of intersection points on the segment line (p1' and p2').
+
+    if len(intersection1) == 2:
+        # forming vectors e1 (segment vector), e2 (segment point to the first intersection point), and e3 (segment point to the second intersection point)
+        e1 = [segment012[3]-segment012[0], segment012[4]-segment012[1]]
+        e2 = [(intersection1[0][0] - segment012[0]), (intersection1[0][1] - segment012[1])]
+        e3 = [(intersection1[1][0] - segment012[0]), (intersection1[1][1] - segment012[1])]
+        # get dot product of e1.e2 and e1.e3
+        valDp1 = sum(map(operator.mul, e1, e2))
+        valDp2 = sum(map(operator.mul, e1, e3))
+        # get length of the vectors
+        lenLineE1 = math.sqrt(e1[0] ** 2 + e1[1] ** 2)
+        lenLineE2 = math.sqrt(e2[0] ** 2 + e2[1] ** 2)
+        lenLineE3 = math.sqrt(e3[0] ** 2 + e3[1] ** 2)
+        # cosign of the angle between vectors e1^e2 and e1^e3
+        cos1 = valDp1 / (lenLineE1 * lenLineE2)
+        cos2 = valDp2 / (lenLineE1 * lenLineE3)
+        # length of v1p1' and v1p2'
+        projLenOfLine1 = cos1 * lenLineE2
+        pp1 = [((segment012[0] + (projLenOfLine1 * e1[0]) / lenLineE1)), ((segment012[1] + (projLenOfLine1 * e1[1]) / lenLineE1))]
+        pp1_2 = [((valDp1 / (lenLineE1 * lenLineE2)) * lenLineE2 * e1[0] / lenLineE1), ((valDp1 / (lenLineE1 * lenLineE2)) * lenLineE2 * e1[1] / lenLineE1)]
+        projLenOfLine2 = cos2 * lenLineE3
+        pp2 =[((segment012[0] + (projLenOfLine2 * e1[0]) / lenLineE2)), ((segment012[1] + (projLenOfLine2 * e1[1]) / lenLineE2))]
+        pp2_2 = [((valDp2 / (lenLineE1 * lenLineE3)) * lenLineE3 * e1[0] / lenLineE1), ((valDp2 / (lenLineE1 * lenLineE3)) * lenLineE3 * e1[1] / lenLineE1)]
+
+    elif len(intersection2) == 2:
+        # forming vectors e1 (segment vector), e2 (segment point to the first intersection point), and e3 (segment point to the second intersection point)
+        e1 = [segment012[3]-segment012[0], segment012[4]-segment012[1]]
+        e2 = [(intersection2[0][0] - segment012[0]), (intersection2[0][1] - segment012[1])]
+        e3 = [(intersection2[1][0] - segment012[0]), (intersection2[1][1] - segment012[1])]
+        # get dot product of e1.e2 and e1.e3
+        valDp1 = sum(map(operator.mul, e1, e2))
+        valDp2 = sum(map(operator.mul, e1, e3))
+        # get length of the vectors
+        lenLineE1 = math.sqrt(e1[0] ** 2 + e1[1] ** 2)
+        lenLineE2 = math.sqrt(e2[0] ** 2 + e2[1] ** 2)
+        lenLineE3 = math.sqrt(e3[0] ** 2 + e3[1] ** 2)
+        # cosign of the angle between vectors e1^e2 and e1^e3
+        cos1 = valDp1 / (lenLineE1 * lenLineE2)
+        cos2 = valDp2 / (lenLineE1 * lenLineE3)
+        # length of v1p1' and v1p2'
+        projLenOfLine1 = cos1 * lenLineE2
+        pp1_2 = [((segment012[0] + (projLenOfLine1 * e1[0]) / lenLineE1)), ((segment012[1] + (projLenOfLine1 * e1[1]) / lenLineE1))]
+        projLenOfLine2 = cos2 * lenLineE3
+        pp2_2 =[((segment012[0] + (projLenOfLine2 * e1[0]) / lenLineE2)), ((segment012[1] + (projLenOfLine2 * e1[1]) / lenLineE2))]
+
+    else:
+        print "No adjacent node on the sidewalk was found!"
+
+    print 'segment:', segment012
+    print 'intersection1:', intersection1
+    print 'intersection2:', intersection2
+    print 'e1, e2, e3:', e1, e2, e3
+    #print 'lenLineE1, lenLineE2, lenLineE3:', lenLineE1, lenLineE2, lenLineE3
+    #print 'cos1, cos2', cos1, cos2
+    #print 'projLenOfLine1, projLenOfLine2:', projLenOfLine1, projLenOfLine2
+    print 'pp1:', pp1
+    print 'pp2:', pp2
+    print 'pp1_2:', pp1_2
+    print 'pp2_2:', pp2_2, '\n'
+    '''
+
+    # for m in distances:
         #if distances.count(m) == 1:
         #distances.remove(m)
         #print distances
